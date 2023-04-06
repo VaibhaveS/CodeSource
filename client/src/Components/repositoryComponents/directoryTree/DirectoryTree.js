65d00cc167890337c8657cf4b973034a7aeea1ca
vaibhaveS <vaibhavedavey@gmail.com>
2023-04-06 18:25:19 +0530

    made it collapsible

--- client/src/Components/repositoryComponents/directoryTree/DirectoryTree.js
+++ client/src/Components/repositoryComponents/directoryTree/DirectoryTree.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './DirectoryTree.css';
 
 const DirectoryTree = ({ tree }) => {
+  const [expanded, setExpanded] = useState({});
+  useEffect(() => {
+    setExpanded({
+      ...expanded,
+      [0]: 1,
+    });
+  }, []);
+  const toggleExpanded = (directoryId) => {
+    console.log(directoryId);
+    setExpanded({
+      ...expanded,
+      [directoryId]: !expanded[directoryId],
+    });
+  };
   const renderTree = (node) => {
     return (
-      <ul>
-        {node.files && node.files.map((file) => <li key={file.fileId}>{file.filename}</li>)}
-        {Object.entries(node)
-          .filter(([key]) => key !== 'files')
-          .map(
-            ([key, value]) =>
-              key !== 'directoryId' && (
-                <li key={value.directoryId}>
-                  {key}
-                  {renderTree(value)}
-                </li>
-              )
-          )}
-      </ul>
+      expanded[node.directoryId] && (
+        <ul>
+          {node.files && node.files.map((file) => <li key={file.fileId}>{file.filename}</li>)}
+          {Object.entries(node)
+            .filter(([key]) => key !== 'files')
+            .map(
+              ([key, value]) =>
+                key !== 'directoryId' && (
+                  <li
+                    key={value.directoryId}
+                    class="folder"
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      toggleExpanded(value.directoryId);
+                    }}
+                  >
+                    {key}
+                    {renderTree(value)}
+                  </li>
+                )
+            )}
+        </ul>
+      )
     );
   };
 
