887e31d938b21b65e863181e3240da06227d5c59
vaibhaveS <vaibhavedavey@gmail.com>
2023-05-01 09:56:36 +0530

    add repo sub-nav

---
+++ client/src/Components/repositoryComponents/code/Code.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Code.css';
+import DirectoryTree from '../directoryTree/DirectoryTree';
+import TextEditor from '../textEditor/TextEditor';
+import ChatWindow from '../../socket/ChatWindow';
+
+const Code = ({ repositoryname, username, repository }) => {
+  const [selected, setSelected] = useState(null);
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        {repository && (
+          <>
+            <div style={{ flex: 1 }}>
+              <TextEditor repo={repository} selected={selected} />
+            </div>
+            <div style={{ flex: 1 }}>
+              <DirectoryTree
+                tree={repository.details.dirTree}
+                selected={selected}
+                setSelected={setSelected}
+              />
+            </div>
+          </>
+        )}
+      </div>
+      <ChatWindow selected={selected || 1} user={username} reponame={repositoryname} />
+    </div>
+  );
+};
+
+export default Code;
--- client/src/Components/repositoryComponents/repoNavbar/RepoNavbar.js
+++ client/src/Components/repositoryComponents/repoNavbar/RepoNavbar.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import './RepoNavbar.css';
+import { Link } from 'react-router-dom';
+import { FaBook, FaCode, FaCodeBranch, FaExclamationCircle, FaCompass } from 'react-icons/fa';
 
-const RepoNavbar = () => {
+const RepoNavbar = ({ reponame, username }) => {
   return (
-    <div>
-      <h1>Repo Navbar</h1>
-    </div>
+    <>
+      <ul className="github">
+        <li className="nav-li">
+          <FaBook size={20} style={{ verticalAlign: 'middle' }} />
+          <h3>
+            <b>VaibhaveS/WhatTODO</b>
+          </h3>
+        </li>
+        <li className="nav-li">
+          <Link to={`/${username}/${reponame}/repository/code`}>
+            <FaCode size={20} style={{ verticalAlign: 'middle' }} />
+            Code
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link to={`/${username}/${reponame}/repository/pull-requests`}>
+            <FaCodeBranch size={20} style={{ verticalAlign: 'middle' }} />
+            Pull Requests
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link to={`/${username}/${reponame}/repository/issues`}>
+            <FaExclamationCircle size={20} style={{ verticalAlign: 'middle' }} />
+            Issues
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link to={`/${username}/${reponame}/repository/explore`}>
+            <FaCompass size={20} style={{ verticalAlign: 'middle' }} />
+            Explore
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
